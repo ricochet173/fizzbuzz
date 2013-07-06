c144bbea8de86a8a1437f0a7128e8e0e97478136
ricochet173 <steve.addona@yahoo.com>
2013-07-06 18:13:03 +0000

    First commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,25 @@
+class FizzBuzz
+
+	def count
+		
+		1.upto(100) do |i|
+			if i % 5 == 0 && i % 3 == 0
+				puts "fizzbuzz"
+			elsif i % 3 == 0
+				puts "fizz"
+			elsif i % 5 == 0
+				puts "buzz"
+			elsif 
+				puts i
+
+			end
+		
+		end
+
+	end
+
+end
+
+# test = FizzBuzz.new
+# test.count
+# You can un-comment the above if you prefer not to bother running it with irb (ruby fizzbuzz.rb)
